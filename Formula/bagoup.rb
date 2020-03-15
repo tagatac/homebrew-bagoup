e642b100b1e0c514400f2858ab780ee38428c077
David Tagatac <david@tagatac.net>
2020-03-15 10:45:36 -0700

    Use vendor directory and correct install location

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -1,14 +1,16 @@
 class Bagoup < Formula
   desc "Mac OS Messages export"
   homepage "https://github.com/tagatac/bagoup"
-  url "https://github.com/tagatac/bagoup/archive/v1.1.1.tar.gz"
-  sha256 "8b082e4b9521a6dac4723ced902fefa53a5e864f1a0fcf7886db3d5daf340f04"
+  url "https://github.com/tagatac/bagoup/archive/v1.1.2.tar.gz"
+  sha256 "ff9b1dd1681b7f81f614dcbeb1c4db699155ea5597f71e3d67e5f5fb309240a2"
 
   depends_on "make" => :build
+  depends_on "go" => :build
 
   def install
     system "make"
-    cp "bagoup", to_s(bin)
+    mkdir bin.to_s
+    cp "bagoup", "#{bin}/bagoup"
   end
 
   test do
