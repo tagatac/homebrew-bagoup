14e57beb073e6b624c657e1a9650ca9c500ef2d9
David Tagatac <david@tagatac.net>
2022-10-07 15:36:04 -0600

    Add monetery bottle

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -5,6 +5,11 @@ class Bagoup < Formula
   sha256 "1b8dcb5d8e2b0a5c859171156ed6af5659f0769089ce47a6164998666ef58f94"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/tagatac/bagoup/releases/download/v2.2"
+    sha256 cellar: :any_skip_relocation, monterey: "2c377d3b1e877ea3a8deaf3137c72763ea38a92006c5dddfa1c031341548554d"
+  end
+
   depends_on "go" => :build
   depends_on "make" => :build
 
