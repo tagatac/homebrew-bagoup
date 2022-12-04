a6d61a83a618d70c1a930b803462a962801e23d6
David Tagatac <david@tagatac.net>
2022-12-03 18:02:12 -0700

    Upgrade to v2.3

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -1,14 +1,14 @@
 class Bagoup < Formula
   desc "Mac OS Messages export"
   homepage "https://github.com/tagatac/bagoup"
-  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.2.tar.gz"
-  sha256 "1b8dcb5d8e2b0a5c859171156ed6af5659f0769089ce47a6164998666ef58f94"
+  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.3.tar.gz"
+  sha256 "06cf4bbf12157faedde3fff874f09e7927039c6e90a76c69353dfbce858498e8"
   license "AGPL-3.0-or-later"
 
-  bottle do
-    root_url "https://github.com/tagatac/bagoup/releases/download/v2.2"
-    sha256 cellar: :any_skip_relocation, monterey: "2c377d3b1e877ea3a8deaf3137c72763ea38a92006c5dddfa1c031341548554d"
-  end
+  # bottle do
+  #   root_url "https://github.com/tagatac/bagoup/releases/download/v2.2"
+  #   sha256 cellar: :any_skip_relocation, monterey: "2c377d3b1e877ea3a8deaf3137c72763ea38a92006c5dddfa1c031341548554d"
+  # end
 
   depends_on "go" => :build
   depends_on "make" => :build
