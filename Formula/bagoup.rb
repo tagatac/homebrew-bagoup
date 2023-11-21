a72c37a213f98e3d85fc34b9a9cc302266e2079d
David Tagatac <david@tagatac.net>
2023-11-21 12:45:14 -0800

    Update to v2.4.1

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -1,14 +1,14 @@
 class Bagoup < Formula
   desc "Mac OS Messages export"
   homepage "https://github.com/tagatac/bagoup"
-  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.3.tar.gz"
-  sha256 "06cf4bbf12157faedde3fff874f09e7927039c6e90a76c69353dfbce858498e8"
+  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.4.1.tar.gz"
+  sha256 "3deae2aa26039b6b7db772927fa7e74ca2d11c7736c98e6cff15793aa72e679d"
   license "AGPL-3.0-or-later"
 
-  bottle do
-    root_url "https://github.com/tagatac/bagoup/releases/download/v2.3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "53934fd81aeb4b7782473f64e0c470ecba977ff6b1fd086359109a07467ad49d"
-  end
+  # bottle do
+  #   root_url "https://github.com/tagatac/bagoup/releases/download/v2.3"
+  #   sha256 cellar: :any_skip_relocation, arm64_ventura: "53934fd81aeb4b7782473f64e0c470ecba977ff6b1fd086359109a07467ad49d"
+  # end
 
   depends_on "go" => :build
   depends_on "make" => :build
@@ -16,7 +16,8 @@ class Bagoup < Formula
   def install
     system "make", "from-archive"
     mkdir bin.to_s
-    cp "bagoup", "#{bin}/bagoup"
+    cp "bin/typedstream-decode", "#{bin}/typedstream-decode"
+    cp "bin/bagoup", "#{bin}/bagoup"
   end
 
   test do
