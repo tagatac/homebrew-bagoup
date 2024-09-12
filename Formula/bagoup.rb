6d3983ca8a5b1c21711480f5467b7ce87493a641
David Tagatac <david@tagatac.net>
2024-09-12 12:43:49 -0700

    Add the arm64_sonoma bottle for v2.4.10

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -5,10 +5,10 @@ class Bagoup < Formula
   sha256 "7d003523c133c7c1453a2b371ea508e7ca95c7df6e504132e22add89c97bd966"
   license "AGPL-3.0-or-later"
 
-  # bottle do
-  #   root_url "https://github.com/tagatac/bagoup/releases/download/v2.4.8"
-  #   sha256 cellar: :any_skip_relocation, arm64_sonoma: "31c41389563f14e948a29cb2806111698704fd0fddaeb450cbf1e0e1d56d21ee"
-  # end
+  bottle do
+    root_url "https://github.com/tagatac/bagoup/releases/download/v2.4.10"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "553626252349efa443060f82008713a1c159a235100ae9eba221802c60c91723"
+  end
 
   depends_on "go" => :build
   depends_on "make" => :build
