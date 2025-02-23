cfdb4115d6854e490c1a905d83288f4816868452
David Tagatac <dtag@amazon.com>
2025-02-23 11:17:46 -0800

    Update to v2.5.0

--- Formula/bagoup.rb
+++ Formula/bagoup.rb
@@ -1,17 +1,18 @@
 class Bagoup < Formula
   desc "Mac OS Messages export"
   homepage "https://github.com/tagatac/bagoup"
-  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.4.11.tar.gz"
-  sha256 "005c864291d8f0998fe33f9fe5fdf57c1fed18ded681d0bca107097dea77855b"
+  url "https://github.com/tagatac/bagoup/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "9b14fdc109838e67f4fb7402f7eda42149457a69246536e9b361b88986ae9d43"
   license "AGPL-3.0-or-later"
 
-  bottle do
-    root_url "https://github.com/tagatac/bagoup/releases/download/v2.4.11"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eecedca9007c3fd30ae3da46316edc0a2be8ccb4bd6a341a7d928d8d9062cd60"
-  end
+  # bottle do
+  #   root_url "https://github.com/tagatac/bagoup/releases/download/v2.4.11"
+  #   sha256 cellar: :any_skip_relocation, arm64_sequoia: "eecedca9007c3fd30ae3da46316edc0a2be8ccb4bd6a341a7d928d8d9062cd60"
+  # end
 
   depends_on "go" => :build
   depends_on "make" => :build
+  depends_on "weasyprint"
 
   def install
     system "make", "from-archive"
